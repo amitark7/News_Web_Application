c2174952deba36e474f21d50358a1ff6a137e79f
amitark7 <amitkumar1600121@gmail.com>
2023-11-05 01:24:47 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,14 @@ app.get('/',(req,res)=>{
 
 app.post('/news',async (req,res)=>{
     const {category,country,page,pageSize}=req.body
-    const url=`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=68cef29466d44cef89e0d613453136b2&page=${page}&pageSize=${pageSize}`
-    const response=await fetch(url)
-    const data=await response.json()
-    res.send(data)
+    try {      
+        const url=`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=68cef29466d44cef89e0d613453136b2&page=${page}&pageSize=${pageSize}`
+        const response=await fetch(url)
+        const data=await response.json()
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen(port,()=>{
