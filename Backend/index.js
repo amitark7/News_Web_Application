8a679aa1a6798bdae5a2e14609758ad11c6399d4
amitark7 <amitkumar1600121@gmail.com>
2023-11-05 00:46:49 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,11 @@ app.use(cors())
 const port=5000
 app.use(express.json())
 
-app.post('/',async (req,res)=>{
+app.get('/',(req,res)=>{
+    res.send("Hello Team How Are You!!")
+})
+
+app.post('/news',async (req,res)=>{
     const {category,country,page,pageSize}=req.body
     const url=`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=68cef29466d44cef89e0d613453136b2&page=${page}&pageSize=${pageSize}`
     const response=await fetch(url)
